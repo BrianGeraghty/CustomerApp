648e80875a581629577ad5aa04bcfa95159ec99d
Brian Geraghty <briangeraghty@gmail.com>
2019-01-19 15:21:49 -0800

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var express = require("express"),
   bodyParser = require("body-parser");
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/VRifydb");
+mongoose.connect("mongodb://localhost/CustomerAppDb");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
